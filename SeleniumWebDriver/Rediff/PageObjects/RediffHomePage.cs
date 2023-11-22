74d33a25153e596baa5162d8d5c00164a74b08a5
ShirinUST <shirin.safwanashiyas@ust.com>
2023-11-22 14:25:02 +0530

    commit

---
+++ SeleniumWebDriver/Rediff/PageObjects/RediffHomePage.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rediff.PageObjects
+{
+    internal class RediffHomePage
+    {
+        IWebDriver? driver;
+        public RediffHomePage(IWebDriver driver) 
+        {
+            this.driver = driver ?? throw new ArgumentException(nameof(driver));
+            PageFactory.InitElements(driver, this);//Pagefactory is a repository
+        }
+        //Arrange
+
+        [FindsBy(How =How.LinkText,Using = "Create Account")]
+        public IWebElement? CreateAccountLink { get; set; }
+
+        [FindsBy(How = How.LinkText, Using = "Sign in")]
+        public IWebElement? SignInLink { get; set; }
+
+        //Act
+        /*
+        public void CreateAccountLinkClick()
+        {
+            CreateAccountLink?.Click();
+        }
+        public void SignInClick()
+        {
+            SignInLink?.Click();
+        }
+        */
+        public CreateAccountPage CreateAccountClick()
+        {
+            CreateAccountLink?.Click();
+            return new CreateAccountPage(driver);
+        }
+    }
+}
+
---
+++ SeleniumWebDriver/Rediff/TestScripts/UserManagementTest.cs
@@ -0,0 +1,69 @@
+﻿using OpenQA.Selenium.Support.UI;
+using Rediff.PageObjects;
+using Rediff.Utilities;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rediff.TestScripts
+{
+    [TestFixture]
+    internal class UserManagementTest:CoreCodes
+    {
+        
+        //Asserts
+        /*
+        [Test, Order(1),Category("Smoke Test")]
+        public void CreateAccountLinkTest()
+        {
+            RediffHomePage homePage=new RediffHomePage(driver);
+            driver.Navigate().GoToUrl("https://www.rediff.com/");
+            //WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            //wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(homePage.CreateAccountLink));
+            homePage.CreateAccountLinkClick();
+           
+            //Console.WriteLine(driver.Title);
+            Console.WriteLine(driver.Url);
+            Thread.Sleep(3000);
+            Assert.That(driver.Url.Contains("register"));
+        }
+        [Test, Order(2),Category("Smoke Test")]
+        public void SignInLinkTest()
+        {
+            
+            var homePage = new RediffHomePage(driver);
+            driver.Navigate().GoToUrl("https://www.rediff.com/");
+            homePage.SignInClick();
+            Thread.Sleep(3000);
+            Assert.That(driver.Url.Contains("login"));
+        }
+        */
+        [Test, Order(1), Category("Regression Test")]
+        public void CreateAccountTest()
+        {
+            var homePage = new RediffHomePage(driver);
+            
+            //WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            //wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(homePage.CreateAccountLink));
+           if(!driver.Url.Equals("https://www.rediff.com/"))
+            {
+                driver.Navigate().GoToUrl("https://www.rediff.com/");
+            }
+            var createaccountpage = homePage.CreateAccountClick();
+
+            //Console.WriteLine(driver.Title);
+           // Console.WriteLine(driver.Url);
+            Thread.Sleep(3000);
+            createaccountpage.FullNameType("XXX");
+            createaccountpage.RediffMailType("XXX");
+            createaccountpage.CheckAvailabilityBtnClick();
+            Thread.Sleep(3000);
+
+            createaccountpage.CreateMyAccountBtnClick();
+            //Assert.That(driver.Url.Contains("register"));
+        }
+    }
+}
